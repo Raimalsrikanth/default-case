916d07cce5b4c14cb49512ba1ccda67cea1e20e6
Raimalsrikanth <96488129+Raimalsrikanth@users.noreply.github.com>
2021-12-30 13:28:02 +0530

    Created default.C
    It is developed by Raimal Srikanth

---
+++ Default.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<Conio.h>
+Void main()
+{
+int k;
+Clrscr();
+Printf("Enter any number")
+Scanf("%d",&k);
+Switch(k)
+{
+Case 0 : printf("\n Number is zero");
+Case 1 :
+Case 2 :
+Case 3 :
+Case 4 : printf("\n Number is less than five");
+break;
+Case 5 : printf("\n Number is greater than five");
+break;
+default : printf("\n Number is greater than five");
+}
+getch();
+}
